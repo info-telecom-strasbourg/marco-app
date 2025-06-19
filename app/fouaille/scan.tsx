1718bd15f36f013b78c4742332310b5f84a951c0
Nicolas Mossmann <n.mossmann@etu.unistra.fr>
2025-06-19 15:17:31 +0200

    Move scan page to fouaille page folder

--- app/fouaille/scan.tsx
+++ app/fouaille/scan.tsx
@@ -4,7 +4,7 @@ import { CameraView, useCameraPermissions, type CameraType } from 'expo-camera';
 import { useState } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
+export default function ScanPage() {
   const [facingDirection, setFacingDirection] = useState<CameraType>('back');
   const [permission, requestPermission] = useCameraPermissions();
 
