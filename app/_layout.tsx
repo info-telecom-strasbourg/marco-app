07d296cfcbfb85f87a714a32aa4e81913143db6e
Nicolas Mossmann <n.mossmann@etu.unistra.fr>
2025-06-26 15:21:09 +0200

    Change pages header title

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInPage from './auth/login';
 
 import HomePage from './(home)/product';
+import CartPage from './(home)/cart';
 
 import ScanPage from './fouaille/scan';
 import { CartProvider } from "../src/store/cart";
@@ -18,10 +19,11 @@ export default function RootLayout() {
       <NavigationContainer>
         <Stack.Navigator initialRouteName="sign_in">
           {/* Global routes */}
-          <Stack.Screen name='sign_in' component={SignInPage} />
+          <Stack.Screen name='sign_in' component={SignInPage} options={{ headerTitle: "Connexion" }} />
 
           {/* Main routes (not admin) */}
-          <Stack.Screen name='home' component={HomePage} />
+          <Stack.Screen name='home' component={HomePage} options={{ headerTitle: "Produits" }} />
+          <Stack.Screen name='cart' component={CartPage} options={{ headerTitle: "Mon panier" }} />
 
           {/* Admin-specific routes (fouaille) */}
           <Stack.Screen name='scan' component={ScanPage} />
