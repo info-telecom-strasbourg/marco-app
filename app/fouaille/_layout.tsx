ee8ea57b223b34aef69a76cbaf2cc65533c37553
Nicolas Mossmann <n.mossmann@etu.unistra.fr>
2026-07-08 04:33:23 +0200

    style(fouaille): Add icon to Tabs layout

--- app/fouaille/_layout.tsx
+++ app/fouaille/_layout.tsx
@@ -1,18 +1,38 @@
+import Icon from "@expo/vector-icons/FontAwesome";
 import { Tabs } from "expo-router";
 
 export default function FouailleLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        sceneStyle: { backgroundColor: "white" },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Fouaille",
+          tabBarIcon: () => <Icon size={16} name="home" />,
         }}
       />
 
-      <Tabs.Screen name="scan" options={{ title: "Scan" }} />
-      <Tabs.Screen name="presets" options={{ title: "Presets" }} />
-      <Tabs.Screen name="[orderId]" options={{ href: null }} />
+      <Tabs.Screen
+        name="scan"
+        options={{
+          title: "Scan",
+          tabBarIcon: () => <Icon size={16} name="camera" />,
+        }}
+      />
+
+      <Tabs.Screen
+        name="presets"
+        options={{
+          title: "Presets",
+          tabBarIcon: () => <Icon size={16} name="address-book" />,
+        }}
+      />
+
+      <Tabs.Screen name="carts/[cartId]" options={{ href: null }} />
     </Tabs>
   );
 }
